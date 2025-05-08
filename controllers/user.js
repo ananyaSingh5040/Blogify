010708039607145a668b06c83e80b73deb1e7ecf
Ananya Singh <singhananya5040@gmail.com>
2025-05-08 14:08:50 +0530

    08-05-25

---
+++ controllers/user.js
@@ -0,0 +1,12 @@
+import User from "../models/user.js";
+export default async function handleUserSignup(req,res) {
+
+    const{name,email,password} = req.body;
+    await User.create({
+        name,
+        email,
+        password,
+
+    });
+    return res.redirect("/");
+}
