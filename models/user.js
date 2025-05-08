6fe1480ec343b53a09348a286b21034be09ca9fe
Ananya Singh <singhananya5040@gmail.com>
2025-05-08 14:08:54 +0530

    08-05-25

---
+++ models/user.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const { createHmac } = await import("node:crypto");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePhotoUrl: {
+      type: String,
+      default: "/public/images/profile.jpg",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function () {
+  const user = this;
+  if (!user.isModified("password")) return;
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest(hex);
+    this.salt = salt;
+    this.password=hashedPassword;
+});
+const User = mongoose.model("user", userSchema);
+export default User;
